8e9057d4fd216225fc4370b0cd998053d43a076d
Michael Harris <mdillanh@gmail.com>
2024-04-14 18:31:28 -0500

    finished activity

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+  # The 'render :home' line is optional here because Rails will
+  # automatically render the 'home' view for the 'home' action
+  render :home
+  end
+  end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  root to: redirect('/home')
+
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
